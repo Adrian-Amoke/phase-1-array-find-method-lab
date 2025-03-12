8f6e5561699fc1a19304c92e5d10330997364916
Adrian <adrian.amoke@gmail.com>
2025-03-12 14:47:17 +0300

    edit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  /*const superbowlWin =record.find(rec => {
+    if (rec === 'W') {
+        return 'a win!';
+    }
+    else {
+        return undefined;
+    }
+});*/
+/*function superbowlWin() {
+    record.find((rec,yr) => {
+        if (rec === 'W') {
+            return 'a win!', yr;
+        }
+        else {
+            return undefined;
+        }
+    });
+}
+console.log(superbowlWin());*/
+function superbowlWin(record) {
+    const win = record.find(rec => rec.result === 'W');
+    return win ? win.year : undefined;
+}
